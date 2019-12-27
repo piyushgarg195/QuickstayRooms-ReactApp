649e620289df474999f23beb87edc4b5759aef0d
Ashmit Mittal <ashmit.mittal111@gmail.com>
2019-12-27 17:00:52 +0530

    Form and crousel

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
 
 ReactDOM.render(
-<<<<<<< HEAD
 	<BrowserRouter>
 		<div>
 			<Navbar />
@@ -23,27 +22,4 @@ ReactDOM.render(
 		</div>
 	</BrowserRouter>,
 	document.getElementById("root")
-=======
-  <BrowserRouter>
-    <div>
-      <Navbar />
-
-      <Switch>
-        {/* Home Route */}
-        <Route exact path="/">
-          <Home />
-        </Route>
-        {/* About Us */}
-        <Route path="/about">
-          <About />
-        </Route>
-        {/* Contact Us */}
-        <Route path="/contact">
-          <About />
-        </Route>
-      </Switch>
-    </div>
-  </BrowserRouter>,
-  document.getElementById("root")
->>>>>>> 58b15cc7f002bb9a76eaa629948b1b2a9e07e897
 );
