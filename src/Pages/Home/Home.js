5170b07a79afbee7aec4355a03acbf7f73d76b38
Piyush Garg <pgarg2598.ca18@chitkara.edu.in>
2019-12-27 16:07:28 +0530

    Bug Fix

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,11 +2,18 @@ import React from "react";
 
 // Custom Components
 import Model from "../../Components/Modals/Model";
+import firebase from "../../firebase";
 
 function Home() {
   return (
     <div className="App">
       <h1>Home Page</h1>
+      <button
+        onClick={() => firebase.auth().signOut()}
+        className="btn btn-danger"
+      >
+        Logout
+      </button>
       {/* <Model /> */}
     </div>
   );
