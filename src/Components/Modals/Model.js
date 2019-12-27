3e27885e2c41fdb64a138e598f92779f0c277ad4
Piyush Garg <pgarg2598.ca18@chitkara.edu.in>
2019-12-27 16:03:07 +0530

    Navbar Updated

--- src/Components/Modals/Model.js
+++ src/Components/Modals/Model.js
@@ -163,12 +163,6 @@ const Model = () => {
                     <div class="btn-wrapper text-center">
                       <a href="#" class="btn btn-neutral btn-icon">
                         <span class="btn-inner--icon">
-                          <img src="../../assets/img/icons/common/github.svg" />
-                        </span>
-                        <span class="btn-inner--text">Github</span>
-                      </a>
-                      <a href="#" class="btn btn-neutral btn-icon">
-                        <span class="btn-inner--icon">
                           <img src="../../assets/img/icons/common/google.svg" />
                         </span>
                         <span class="btn-inner--text">Google</span>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,7 +7,7 @@ function Home() {
   return (
     <div className="App">
       <h1>Home Page</h1>
-      <Model />
+      {/* <Model /> */}
     </div>
   );
 }
