767f7c950fcc894b6415e922b816f1a37d6df8ec
Ashmit Mittal <ashmit.mittal111@gmail.com>
2019-12-29 17:24:59 +0530

    revert back

--- src/Components/Filter/Filter-details.js
+++ src/Components/Filter/Filter-details.js
@@ -1,58 +1,55 @@
 import React from "react";
 import "./Filter-details.css";
 
-function Filter(props) {
-  return (
-    <div>
-      <div className="filter-cotanier">
-        <div className="row">
-          <div className="col-sm-2">
-            <div class="form-group">
-              <div class="input-group input-group-alternative">
-                <div class="input-group-prepend">
-                  <span class="input-group-text">
-                    <i class="ni ni-calendar-grid-58"></i>
-                  </span>
-                </div>
-                <input
-                  class="form-control datepicker"
-                  placeholder="Select date"
-                  type="text"
-                  value="06/20/2019"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-2">
-            <div className="form-group">
-              <select
-                onChange={e => props.handelPgChange(e.target.value)}
-                className="form-control"
-              >
-                <option value="boys">Boys</option>
-                <option value="girls">Girls</option>
-              </select>
-            </div>
-          </div>
-          <div className="col-sm-2">
-            <div className="form-group">
-              <select className="form-control">
-                <option>Food</option>
-                <option>No Food</option>
-              </select>
-            </div>
-          </div>
-          <div className="col-sm-2">
-            <div className="form-group">
-              <select className="form-control">
-                <option>Locality</option>
-              </select>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+function Filter() {
+	return (
+		<div>
+			<div className='filter-cotanier'>
+				<div className='row'>
+					<div className='col-sm-2'>
+						<div class='form-group'>
+							<div class='input-group input-group-alternative'>
+								<div class='input-group-prepend'>
+									<span class='input-group-text'>
+										<i class='ni ni-calendar-grid-58'></i>
+									</span>
+								</div>
+								<input
+									class='form-control datepicker'
+									placeholder='Select date'
+									type='text'
+									value='06/20/2019'
+								/>
+							</div>
+						</div>
+					</div>
+					<div className='col-sm-2'>
+						<div className='form-group'>
+							<select className='form-control'>
+								<option>Boys</option>
+								<option>Girls</option>
+							</select>
+						</div>
+					</div>
+					<div className='col-sm-2'>
+						<div className='form-group'>
+							<select className='form-control'>
+								<option>Food</option>
+								<option>No Food</option>
+							</select>
+						</div>
+					</div>
+					<div className='col-sm-2'>
+						<div className='form-group'>
+							<select className='form-control'>
+								<option>Locality</option>
+							</select>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Filter;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,116 +4,116 @@ import React, { useState, useEffect } from "react";
 import firebase from "../../firebase";
 
 const Navbar = () => {
-  const [isSignedIn, setSignIn] = useState(false);
-  const [user, setUser] = useState({});
+	const [isSignedIn, setSignIn] = useState(false);
+	const [user, setUser] = useState({});
 
-  const signInHandler = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then(function(result) {
-        if (result.user) {
-          setUser(user);
-          setSignIn(true);
-        } else {
-          setSignIn(false);
-        }
-      });
-  };
+	const signInHandler = () => {
+		const provider = new firebase.auth.GoogleAuthProvider();
+		firebase
+			.auth()
+			.signInWithPopup(provider)
+			.then(function(result) {
+				if (result.user) {
+					setUser(user);
+					setSignIn(true);
+				} else {
+					setSignIn(false);
+				}
+			});
+	};
 
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        setUser(user);
-        setSignIn(true);
-      } else {
-        setSignIn(false);
-      }
-    });
-  });
+	useEffect(() => {
+		firebase.auth().onAuthStateChanged(function(user) {
+			if (user) {
+				setUser(user);
+				setSignIn(true);
+			} else {
+				setSignIn(false);
+			}
+		});
+	});
 
-  return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary ">
-      <div class="container">
-        <a class="navbar-brand" href="#">
-          Quickstay
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbar-primary"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbar-primary">
-          <div class="navbar-collapse-header">
-            <div class="row">
-              <div class="col-6 collapse-brand">
-                <a href="index.html">
-                  {/* <img src='assets/img/brand/blue.png' /> */}
-                </a>
-              </div>
-              <div class="col-6 collapse-close">
-                <button
-                  type="button"
-                  class="navbar-toggler"
-                  data-toggle="collapse"
-                  data-target="#navbar-primary"
-                  aria-controls="navbar-primary"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span></span>
-                  <span></span>
-                </button>
-              </div>
-            </div>
-          </div>
+	return (
+		<nav class='navbar navbar-expand-lg navbar-dark bg-primary '>
+			<div class='container'>
+				<a class='navbar-brand' href='#'>
+					Quickstay
+				</a>
+				<button
+					class='navbar-toggler'
+					type='button'
+					data-toggle='collapse'
+					data-target='#navbar-primary'
+					aria-controls='navbar-default'
+					aria-expanded='false'
+					aria-label='Toggle navigation'
+				>
+					<span class='navbar-toggler-icon'></span>
+				</button>
+				<div class='collapse navbar-collapse' id='navbar-primary'>
+					<div class='navbar-collapse-header'>
+						<div class='row'>
+							<div class='col-6 collapse-brand'>
+								<a href='index.html'>
+									<img src='assets/img/brand/blue.png' />
+								</a>
+							</div>
+							<div class='col-6 collapse-close'>
+								<button
+									type='button'
+									class='navbar-toggler'
+									data-toggle='collapse'
+									data-target='#navbar-primary'
+									aria-controls='navbar-primary'
+									aria-expanded='false'
+									aria-label='Toggle navigation'
+								>
+									<span></span>
+									<span></span>
+								</button>
+							</div>
+						</div>
+					</div>
 
-          <ul class="navbar-nav ml-lg-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Home
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                About
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Blog
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Partner with us
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              {!isSignedIn ? (
-                <a class="nav-link" onClick={() => signInHandler()} href="#">
-                  Sign In
-                  <span class="sr-only">(current)</span>
-                </a>
-              ) : (
-                <a class="nav-link" onClick={() => {}} href="#">
-                  {user.displayName}
-                  <span class="sr-only">(current)</span>
-                </a>
-              )}
-            </li>
-            {/* <li class='nav-item dropdown'>
+					<ul class='navbar-nav ml-lg-auto'>
+						<li class='nav-item'>
+							<a class='nav-link' href='#'>
+								Home
+								<span class='sr-only'>(current)</span>
+							</a>
+						</li>
+						<li class='nav-item'>
+							<a class='nav-link' href='#'>
+								About
+								<span class='sr-only'>(current)</span>
+							</a>
+						</li>
+						<li class='nav-item'>
+							<a class='nav-link' href='#'>
+								Blog
+								<span class='sr-only'>(current)</span>
+							</a>
+						</li>
+						<li class='nav-item'>
+							<a class='nav-link' href='#'>
+								Partner with us
+								<span class='sr-only'>(current)</span>
+							</a>
+						</li>
+						<li class='nav-item'>
+							{!isSignedIn ? (
+								<a class='nav-link' onClick={() => signInHandler()} href='#'>
+									Sign In
+									<span class='sr-only'>(current)</span>
+								</a>
+							) : (
+								<a class='nav-link' onClick={() => {}} href='#'>
+									{user.displayName}
+									<span class='sr-only'>(current)</span>
+								</a>
+							)}
+						</li>
+						{/* <li class='nav-item dropdown'>
 							<a
 								class='nav-link'
 								href='#'
@@ -141,11 +141,11 @@ const Navbar = () => {
 								</a>
 							</div>
 						</li> */}
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
